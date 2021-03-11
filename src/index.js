9b472ec2eb167af8e87ad83c46ce4e52e2813bab
jsimanca <jsimanca@grupodot.com>
2021-03-11 16:28:18 -0500

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-const http = require('http');
-const path = require('path');
 const express = require('express');
-const socketio = require('socket.io');
+const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const path = require('path');
 
 const mongoose = require('mongoose');
 
-const app = express();
 app.set('trust proxy', true);
-const server = http.createServer(app);
-const io = socketio.listen(server);
 
 //db connection
-
-mongoose.connect('mongodb+srv://jonathin:jonathin@cluster0.ujuax.mongodb.net/chat-db?retryWrites=true&w=majority')
-  .then(db => console.log('db ok'))
-  .catch(err => console.log('db err'))
+mongoose.connect('mongodb+srv://jonathin:jonathin@cluster0.ujuax.mongodb.net/chat-db?retryWrites=true&w=majority',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(db => console.log('db ok')).catch(err => console.log('db err'))
 
 //Settings
 app.set('port', process.env.PORT || 3000);
